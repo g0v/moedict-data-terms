14b397868819d808105202df5686711777821452
Poren Chiang <poren@poren.tw>
2023-04-11 17:48:00 +0800

    Parse category update time as datetime object

--- src/load_categories.py
+++ src/load_categories.py
@@ -5,15 +5,25 @@ import re
 import requests
 import sys
 from bs4 import BeautifulSoup
+from datetime import datetime
 from urllib.parse import urljoin
 
 NAER_BASE_URL = 'https://terms.naer.edu.tw'
 NAER_INDEX_URL = 'https://terms.naer.edu.tw/download/1/?page_size=1000'
 
-def load_categories(file=None) -> list[tuple[str, str, str, str]]:
+date_format_expr = re.compile(r'(?P<year>\d{4})年(?P<month>\d{1,2})月(?P<day>\d{1,2})日\s+(?P<hour>0?\d|\d{2}):(?P<minute>\d{2})')
+
+def parse_date(string: str) -> datetime:
+    if m := date_format_expr.fullmatch(string):
+        year, month, day, hour, minute = int(m['year']), int(m['month']), int(m['day']), int(m['hour']), int(m['minute'])
+        return datetime(year, month, day, hour, minute)
+    raise ValueError('Failed to parse string as date')
+
+
+def load_categories(file=None) -> list[tuple[str, str, datetime, str]]:
     """
     Downloads all the categories listed on the Download page of NAER.
-    Returns a list of tuples (ID, name, last updated on).
+    Returns a list of tuples (ID, name, last updated on, data file URL).
     """
 
     # Defaults to stdout
@@ -26,17 +36,17 @@ def load_categories(file=None) -> list[tuple[str, str, str, str]]:
     for row in soup.css.select('#pageContent div.table-rwd > div.tbody > div.tr'):
         cat_id = int(row.select_one('input[name="num"]')['value'])
         name = row.select_one('div.td[aria-label="領域類別"]').string.strip()
-        last_updated = row.select_one('div.td[aria-label="更新日期"]').string.strip()
+        last_updated = parse_date(row.select_one('div.td[aria-label="更新日期"]').string.strip())
         data_url = urljoin(NAER_BASE_URL, row.select_one('a[title="下載"]')['href'])
 
         categories.append((cat_id, name, last_updated, data_url))
-        print(cat_id, name, last_updated, data_url, sep=',', file=file)
+        print(cat_id, name, last_updated.isoformat(sep=' '), data_url, sep=',', file=file)
 
     return categories
 
 
 def print_usage():
-    print('Usage: load_categories.py [-]')
+    print('Usage: src/load_categories.py [-]')
 
 
 if __name__ == '__main__':
